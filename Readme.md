7698b0fe5abc1c88256656f213a88d71e7262306
Camilo <kaiser130@hotmail.com>
2022-05-29 23:51:35 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Fetching data from local json-server
+
+_if you want to run this project you will have to run the next commands_
+
+`npm run api-local`
+
+**Still under construction**
+
+MejiaLC :tw-1f603:
---
+++ app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  getApiData();
+});
+
+const getApiData = async () => {
+  let response = await fetch("http://localhost:3000/users");
+  let data = await response.json();
+
+  console.log(data);
+
+  return data;
+};
+
+let btnNewData = document.getElementById("sendNewData");
+
+const newDataUser = async (e) => {
+  e.preventDefault();
+  let newPerson = {
+    id: Math.random() * 10,
+    name: "Luis",
+    age: 90,
+    email: "luis@hotmail.com",
+  };
+
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPerson),
+  };
+
+  let response = await fetch("http://localhost:3000/users", options);
+  let data = await response.json();
+
+  return data;
+};
+
+btnNewData.addEventListener("click", newDataUser);
